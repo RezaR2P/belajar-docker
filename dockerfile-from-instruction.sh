1eaec83d457e004bcc6f5b582d5830464b6180c2
RezaR2P <kecilreza575@gmail.com>
2024-08-28 17:37:49 +0700

    Menghapus file yang tidak diperlukan

--- dockerfile-from-instruction.sh
+++
@@ -1,5 +0,0 @@
-Saat kita membuat Docker Image, biasanya perintah pertama adalah melakukan build stage dengan instruksi FROM
-FROM digunakan untuk membuat build stage dari image yang kita tentukan
-Biasanya, jarang sekali kita akan membuat Docker Image dari scratch (kosongan), biasanya kita akan membuat Docker Image dari Docker Image lain yang sudah ada
-Untuk menggunakan FROM, kita bisa gunakan perintah :
-FROM image:version
